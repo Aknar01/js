d283243eb3ef39768503c3f9798877f9973004c3
Aknar01 <96565125+Aknar01@users.noreply.github.com>
2022-02-03 15:23:14 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+document.write("Aknar" + " " + "CS-2117");
+document.write("<br />" + "878 days left until the freedom!");
+
+
+(function (global) {
+    "use strict";
+    function Clock(el) {
+        var document = global.document;
+        this.el = document.getElementById(el);
+        this.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        this.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    }
+    Clock.prototype.addZero = function (i) {
+        if (i < 10) {
+            i = "0" + i;
+            return i;
+        }
+        return i;
+    };
+    Clock.prototype.updateClock = function () {
+        var now, year, month, dayNo, day, hour, minute, second, result, self;
+        now = new global.Date();
+        year = now.getFullYear();
+        month = now.getMonth();
+        dayNo = now.getDay();
+        day = now.getDate();
+        hour = this.addZero(now.getHours());
+        minute = this.addZero(now.getMinutes());
+        second = this.addZero(now.getSeconds());
+        result = this.days[dayNo] + ", " + day + " " + this.months[month] + " " + year + " " + hour + ":" + minute + ":" + second;
+        self = this;
+        self.el.innerHTML = result;
+        global.setTimeout(function () {
+            self.updateClock();
+        }, 1000);
+    };
+    global.Clock = Clock;
+}(window));
+
+function addEvent(elm, evType, fn, useCapture) {
+    "use strict";
+    if (elm.addEventListener) {
+        elm.addEventListener(evType, fn, useCapture);
+    } else if (elm.attachEvent) {
+        elm.attachEvent('on' + evType, fn);
+    } else {
+        elm['on' + evType] = fn;
+    }
+}
+
+addEvent(window, "load", function () {
+    if (document.getElementById("clock")) {
+        var clock = new Clock("clock");
+        clock.updateClock();
+    }
+});
+
+
+function multiplyBy(){
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy() { 
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 / num2;
+}
+
